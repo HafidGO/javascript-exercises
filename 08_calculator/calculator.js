9518b159ef296bd76e9e9c7b47c00dd8aa700390
HafidGO <22013428@es.uveg.edu.mx>
2025-01-30 19:59:55 -0600

    First possible solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+  (typeof(firstNumber === 'number') && typeof(secondNumber)) ==='number' ? (firstNumber + secondNumber) : 'Not a number';
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+  (typeof(firstNumber === 'number') && typeof(secondNumber)) ==='number' ? (firstNumber - secondNumber) : 'Not a number';
 };
 
-const sum = function() {
-	
+const sum = function(firstNumber, secondNumber) {
+  (typeof(firstNumber === 'number') && typeof(secondNumber)) ==='number' ? (firstNumber + secondNumber) : 'Not a number';
 };
 
-const multiply = function() {
-
+const multiply = function(firstNumber, secondNumber) {
+  (typeof(firstNumber === 'number') && typeof(secondNumber)) ==='number' ? (firstNumber * secondNumber) : 'Not a number';
 };
 
-const power = function() {
-	
+const power = function(firstNumber, secondNumber) {
+  if (typeof(firstNumber === 'number') && typeof(secondNumber) === 'number'){
+    const acummulator = firstNumber;
+    while(acummulator<=secondNumber){
+      acummulator *= firstNumber;
+  }
+  return acummulator
+  }
+  else{
+    return 'not a number'
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(firstNumber) {
+  if (typeof(firstNumber === 'number') === 'number'){
+    const counter = firstNumber;
+    while(acummulator >= 1){
+      firstNumber *= couter;
+      counter--;
+  }
+  return acummulator
+  }
+  else{
+    return 'not a number'
+  }
 };
 
 // Do not edit below this line
