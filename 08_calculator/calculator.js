ad631a236953a3419b85e1fc07c54ae7cbab774b
HafidGO <106498821+HafidGO@users.noreply.github.com>
2025-01-31 19:36:33 -0600

    Calculator script working

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,40 +1,48 @@
 const add = function(firstNumber, secondNumber) {
-  (typeof(firstNumber === 'number') && typeof(secondNumber)) ==='number' ? (firstNumber + secondNumber) : 'Not a number';
-};
-
+  return (typeof firstNumber === 'number' && typeof secondNumber ==='number')
+  ? firstNumber + secondNumber 
+  : "Not a number";
+}
+  
 const subtract = function(firstNumber, secondNumber) {
-  (typeof(firstNumber === 'number') && typeof(secondNumber)) ==='number' ? (firstNumber - secondNumber) : 'Not a number';
+  return (typeof firstNumber === 'number' && typeof secondNumber ==='number')
+  ? (firstNumber - secondNumber) 
+  : 'Not a number';
 };
 
-const sum = function(firstNumber, secondNumber) {
-  (typeof(firstNumber === 'number') && typeof(secondNumber)) ==='number' ? (firstNumber + secondNumber) : 'Not a number';
-};
+const sum = function(arrayOfNumbers){
+    return arrayOfNumbers.reduce((accumulator, currentValue) => 
+    accumulator + currentValue, 0)
+}
 
-const multiply = function(firstNumber, secondNumber) {
-  (typeof(firstNumber === 'number') && typeof(secondNumber)) ==='number' ? (firstNumber * secondNumber) : 'Not a number';
-};
+const multiply = function(arrayOfNumbers) {
+return arrayOfNumbers.reduce((accumulator, currentValue) => 
+  accumulator * currentValue)
+}
 
 const power = function(firstNumber, secondNumber) {
   if (typeof(firstNumber === 'number') && typeof(secondNumber) === 'number'){
-    const acummulator = firstNumber;
-    while(acummulator<=secondNumber){
-      acummulator *= firstNumber;
+    var accumulator = firstNumber;
+    var counter = 1
+    while(counter<secondNumber){
+      accumulator *= firstNumber;
+      counter++;
   }
-  return acummulator
-  }
-  else{
-    return 'not a number'
+    return accumulator
   }
 };
 
 const factorial = function(firstNumber) {
-  if (typeof(firstNumber === 'number') === 'number'){
-    const counter = firstNumber;
-    while(acummulator >= 1){
-      firstNumber *= couter;
+  if (firstNumber === 0) {
+    return 1;
+  }
+  else if (typeof firstNumber === 'number'){
+    let counter = firstNumber-1;
+    while(counter > 1){
+      firstNumber *= counter;
       counter--;
   }
-  return acummulator
+  return firstNumber
   }
   else{
     return 'not a number'
