ec2d4e4ec9566a05ed8468301cd1613112afb864
HafidGO <106498821+HafidGO@users.noreply.github.com>
2025-02-03 20:46:44 -0600

    09_palindromes solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (userString) {
+    let cleanedString = userString.toLowerCase().replace(/[^a-z0-9]/g, ''); // Remove non-alphanumeric characters
+    let reverseString = cleanedString.split('').reverse().join('');
 
+    return cleanedString === reverseString;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
