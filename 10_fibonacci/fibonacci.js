02aa1b78b6d810956a38943bb36e69592d916225
HafidGO <106498821+HafidGO@users.noreply.github.com>
2025-02-05 19:47:40 -0600

    Working on 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number !== "number"){
+        number = parseInt(number);
+    }
+    
+    if (number = 0) return 0;
+    if (number < 0) return "OOPS";
+    
 
+    let firstNumber = 0;
+    let secondNumber = 1;
+
+    for (let index = 0; index < number; index++) {
+        let current =  firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
