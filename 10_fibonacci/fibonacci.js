600bccb04cb40ff21b773ec7669737b5571554ba
HafidGO <22013428@es.uveg.edu.mx>
2025-02-05 20:41:55 -0600

    10_fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,22 +1,21 @@
 const fibonacci = function(number) {
-    if(number !== "number"){
+    if (typeof number !== "number"){
         number = parseInt(number);
     }
     
-    if (number = 0) return 0;
+    if (number == 0) return 0;
     if (number < 0) return "OOPS";
     
-
     let firstNumber = 0;
     let secondNumber = 1;
 
-    for (let index = 0; index < number; index++) {
+    for (let index = 2; index <= number; index++) {
         let current =  firstNumber + secondNumber;
         firstNumber = secondNumber;
         secondNumber = current;
     }
 
-    return current;
+    return secondNumber;
 };
 
 // Do not edit below this line
